13f5e885f4b5668aab8a10d2747084490517390f
0xJonaseb11 <sebejaz99@gmail.com>
2024-11-10 00:48:16 +0200

    update #BooksRegistry

---
+++ contracts/BooksRegistry.js
@@ -0,0 +1,32 @@
+class BooksRegistry {
+    constructor () {
+        this.books = {}; // mapping bool IDs to details
+    }
+
+    // Register a book in the system
+    async registerBook (bookId, title, author, isbn) {
+        if (!this.books[bookId]) {
+            this.books[bookId] = {title, author, isbn, status: "available"};
+            return `Book ${title} by ${author} registered with ID: ${bookId}`;
+        }
+        return `Book with ID: ${bookId} already exists!!`;
+    }
+
+    // update book information
+    async updateBool(bookId, title, author, isbn) {
+        const book = this.books[bookId];
+        if (book) {
+            book.title = title || book.title;
+            book.author = author || book.author;
+            book.isbn = isbn || book.usbn;
+            return `Book with ID: ${bookId} not found!`;
+        }
+
+        // Get all books
+        async GetAllBooks() {
+            return this.books;
+        }
+    }
+}
+
+module.exports = BooksRegistry;r
